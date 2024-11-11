933e2d05b5bddbb3b788ae38f447d75fcf1b54a9
x9laaa <acarreno95@outlook.com>
2024-11-11 09:25:04 -0300

    cambio en constants

--- app/src/main/java/cl/bootcamp/newsapi/data/ApiNews.kt
+++ app/src/main/java/cl/bootcamp/newsapi/data/ApiNews.kt
@@ -2,12 +2,13 @@ package cl.bootcamp.newsapi.data
 
 import cl.bootcamp.newsapi.modal.NewsModal
 import cl.bootcamp.newsapi.util.Constants.Companion.API_KEY
+import cl.bootcamp.newsapi.util.Constants.Companion.ENDPOINT
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ApiNews {
-    @GET("everything")
+    @GET(ENDPOINT)
     suspend fun getNews(
         @Query("q") query: String,
         @Query("apiKey") apiKey: String = API_KEY
--- app/src/main/java/cl/bootcamp/newsapi/util/Constants.kt
+++ app/src/main/java/cl/bootcamp/newsapi/util/Constants.kt
@@ -3,7 +3,7 @@ package cl.bootcamp.newsapi.util
 class Constants {
     companion object {
         const val BASE_URL = "https://newsapi.org/v2/"
-        const val ENDPOINT_PALABRA = "everything"
+        const val ENDPOINT = "everything"
         const val API_KEY = "21e9e7055177408a815da315f49f1483"  // No incluyas "apiKey=" aquí, se usará en la consulta
     }
 }
